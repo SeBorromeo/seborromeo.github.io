ef999f53a5aac2e6507a208c6ed0a788017281e4
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-05-23 13:27:40 -0400

    Text of first section and view resume button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 
 import Navbar from "@/components/layout/Navbar";
 import AppearingText from "@/components/ui/AppearingText";
-import Image from 'next/image';
-import section1Gradient from "../../public/images/section1-gradient.svg";
 
 import styles from "./page.module.scss";
 
@@ -13,14 +11,15 @@ export default function Home() {
       <Navbar/>
       <main className={styles.main}>
         <div className={styles.section1}>
-          <AppearingText text="Hi, I'm Sebastian!" className={styles.name}/>
-          <h2 className={styles.title}>COMPUTER SCIENCE STUDENT AT THE UNIVERSITY OF VIRGINIA</h2>
-          <Image
-            priority
-            src={section1Gradient}
-            alt=""
-            className={styles.section1_background}
-          />
+          <div className={styles.name_container}>
+            <AppearingText text="Hi, I'm Sebastian!" className={styles.name}/>
+            <h2 className={styles.title}>COMPUTER SCIENCE STUDENT AT THE UNIVERSITY OF VIRGINIA</h2>
+            <h3 className={styles.bio}>Aspiring Software Engineer familiar in HTML, CSS, SASS, JavaScript, TypeScript, React, Nextjs, PHP, SQL, and RESTful APIs.</h3>
+            <a href="/images/resume.pdf" className={styles.resume_button}>VIEW MY RESUME</a>
+          </div>
+          <div className={styles.photo_container}>
+
+          </div>
         </div>
       </main>
     </>
