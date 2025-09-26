15e1cd7db8bcef938d5db86380e75375f4c39def
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-09-26 04:23:12 -0400

    Button ui comp with unique hover anim

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React, { CSSProperties, FC, ReactNode } from 'react';
+
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+    children: ReactNode;
+    style?: CSSProperties;
+    className?: string;
+    href: string;
+}
+
+const Button: FC<ButtonProps> = ({
+    children,
+    style,
+    className = '',
+    href = '#',
+}) => {
+    return (
+        <a
+            href={href}
+            className={`${styles.utl_button} ${className}`}
+            style={style}
+        >
+            <div className={styles.utl_button_text_container}>
+                <span className={styles.utl_button_text_wrapper}>
+                    {children}
+                </span>
+                <span className={styles.utl_button_text_wrapper} aria-hidden="true">
+                    {children}
+                </span>
+            </div>
+        </a>
+    );
+};
+
+export default Button;
