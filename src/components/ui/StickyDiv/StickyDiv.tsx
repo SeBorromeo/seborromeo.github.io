d74611f0ee17a58779c2750904a646b7392a9ad5
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-09-27 12:38:41 -0400

    Changed stickyDiv behavior, more customizable ease factor

--- src/components/ui/StickyDiv/StickyDiv.tsx
+++ src/components/ui/StickyDiv/StickyDiv.tsx
@@ -9,27 +9,46 @@ import styles from './StickyDiv.module.scss';
 interface StickyDivProps {
     children: ReactNode;
     padding?: string;
+    easeFactor?: number;
 }
 
-const StickyDiv: FC<StickyDivProps> = ({ children, padding = '10px'}) => {
+const StickyDiv: FC<StickyDivProps> = ({ children, padding = '10px', easeFactor = 0.8 }) => {
     const container = useRef<HTMLDivElement>(null);
     const setX = useRef<gsap.QuickToFunc | null>(null);
     const setY = useRef<gsap.QuickToFunc | null>(null);
 
     const { contextSafe } = useGSAP();
 
-
     const onMouseEnter = contextSafe(() => {
-        gsap.to(container.current, { yPercent: -50, xPercent: -50 });
         setX.current = gsap.quickTo(container.current, "x", {duration: 0.6, ease: "power3"});
         setY.current = gsap.quickTo(container.current, "y", {duration: 0.6, ease: "power3"});
     });
 
+    const easeInOut = (t: number) => easeFactor * Math.sin(t);
+
     const onMouseMove = contextSafe((e: React.MouseEvent) => {
-        let rect = container.current?.getBoundingClientRect();
+        let parent = container.current?.parentElement?.getBoundingClientRect();
         
-        setX.current?.(e.clientX - (rect ? rect.left: 0));
-        setY.current?.(e.clientY - (rect ? rect.top: 0));
+        if (parent) {
+            const mx = e.clientX - parent.left;
+            const my = e.clientY - parent.top;
+            
+            const w = parent.width;
+            const h = parent.height;
+
+            // normalized distance from center [-0.5,0.5]
+            let px = mx / parent.width - 0.5; 
+            let py = my / parent.height - 0.5; 
+
+            px = easeInOut(px); 
+            py = easeInOut(py); 
+            
+            const offsetX = -w * px; 
+            const offsetY = -h * py; 
+
+            setX.current?.(mx + offsetX - w / 2); 
+            setY.current?.(my + offsetY - h / 2);
+        }
     });     
 
     const onMouseLeave = contextSafe(() => {
