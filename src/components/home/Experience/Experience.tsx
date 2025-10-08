b9867711b264390da39ece65ecd93ffef5f858d7
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-10-07 20:26:52 -0400

    Adding styling, animation, amd imgs to experience

--- src/components/home/Experience/Experience.tsx
+++ src/components/home/Experience/Experience.tsx
@@ -1,4 +1,7 @@
 import { prisma } from '@/lib/prisma';
+import Tags from '@/components/ui/Tags/Tags';
+import MaskedTextReveal from '@/components/ui/animations/MaskedTextReveal/MaskedTextReveal';
+import Image from 'next/image';
 
 import styles from './Experience.module.scss';
 
@@ -27,15 +30,36 @@ export default async function Experience() {
         <section className={styles.experience_section}>
             <div className={styles.experience_logos_column_bg}/>
             <div className={styles.experience_column}>
-                    {experiences.map((exp: Experience) => (
-                        <div key={exp.id} className={styles.experience_card}>
-                        <h3>{exp.role} @ {exp.company}</h3>
-                        <p>{exp.description}</p>
-                        <p>Skills: {exp.skills.join(", ")}</p>
+                <h1>Experience</h1>
+                {experiences.map((exp: Experience) => (
+                    <div key={exp.id} className={styles.experience_row}>
+                        <div className={styles.text_section}>
+                            <div className={styles.time_tags_column}>
+                                <p>{exp.startDate.toLocaleString('en-US', { month: 'short', year: 'numeric'})} - {exp.endDate ? exp.endDate.toLocaleString('en-US', { month: 'short', year: 'numeric'}) : "Present"}</p>
+                                <Tags list={exp.skills} />
+                            </div>
+                            <div className={styles.role_description_column}>
+                                <MaskedTextReveal>
+                                    <h3>{exp.role} @ {exp.company}</h3>
+                                </MaskedTextReveal>
+                                <MaskedTextReveal>
+                                    <p>{exp.description}</p>
+                                </MaskedTextReveal>
+                            </div>
                         </div>
-                    ))}
+                        <div className={styles.logo_section}>
+                            <div className={styles.logo_container}>
+                                <Image
+                                    fill
+                                    src={exp.logoUrl}
+                                    alt="Photo of Sebastian"
+                                    style={{ objectFit: "cover" }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-
         </section>
     );
 };
