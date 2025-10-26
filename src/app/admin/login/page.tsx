8589ec403f45261c536a5e5f2b5a94c9a4c8bc22
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-10-26 19:14:27 -0400

    Switched login form to use server action

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useActionState, useState } from 'react';
+import styles from './page.module.scss';
+import { login } from '@/app/actions/auth';
+
+export default function LoginPage() {
+  const [state, action, pending] = useActionState(login, undefined)
+
+  return (
+    <div className={styles.container}>
+      <form action={action} className={styles.form}>
+        <h2 className={styles.title}>Login</h2>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input className={styles.input} id="email" name="email" type="email" placeholder="Email" required/>
+        </div>
+        {state?.errors?.email && <p className={styles.error}>{state.errors.email}</p>}
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" placeholder="Password" required/>
+        </div>
+        {state?.errors?.password && <p className={styles.error}>{state.errors.password}</p>}
+
+        <button type="submit" className={styles.button} disabled={pending}>
+          {pending ? 'Logging in...' : 'Login'}
+        </button>
+      </form>
+    </div>
+  );
+}
