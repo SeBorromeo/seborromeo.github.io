e4a514c725b60f67462a0f07351bcbc31befac87
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-11-03 14:02:24 -0500

    Removed static export in config and finally fixed sass transition from imports to use

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: '',
-    output: 'export',
     reactStrictMode: true,
-    images: { 
-        unoptimized: true, // For static export
-    },
 };
 
 export default nextConfig;
