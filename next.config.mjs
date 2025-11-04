02035028e5912e35e5c3297fb267efe52f366339
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-11-04 17:34:54 -0500

    added img remotePatterns to aws bucket

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            new URL('https://raw.githubusercontent.com/oyfaatuva/oyfa-website/**'),
+            new URL('https://seborromeo-portfolio-assets.s3.us-east-2.amazonaws.com/images/**')
+        ],
+    },
 };
 
 export default nextConfig;
