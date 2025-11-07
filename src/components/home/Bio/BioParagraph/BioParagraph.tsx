b6b6c248680b39ff54d62af565ecf0a5af3f89e4
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-11-07 10:30:04 -0500

    cleaned up old richText type and usage

--- src/components/home/Bio/BioParagraph/BioParagraph.tsx
+++ src/components/home/Bio/BioParagraph/BioParagraph.tsx
@@ -1,17 +1,7 @@
 import FadeInDiv from "@/components/ui/animations/FadeInDiv/FadeInDiv";
 import { JSONContent } from '@tiptap/react'
 import { prisma } from "@/lib/prisma"
-import { RichTextNode } from "@/types/richText";
 import { jsonToHTML } from "@/lib/tiptap";
-
-export type Paragraph = RichTextNode[];
-
-export type Bio = {
-    id: string;
-    paragraphs: Paragraph[];
-    createdAt: Date;
-    updatedAt: Date;
-};
  
 export default async function BioParagraph() {
     const bio = await prisma.bio.findFirst({
--- src/types/richText.ts
+++
@@ -1,5 +0,0 @@
-export type RichTextNode =
-  | { type: 'text'; text: string }
-  | { type: 'bold'; text: string }
-  | { type: 'italic'; text: string }
-  | { type: 'link'; url: string; text: string };
