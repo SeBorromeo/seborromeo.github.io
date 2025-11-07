806b6042e9ed83e546f704c43ac5fd64636e3de4
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-11-06 22:04:57 -0500

    Deleted BioEditor comp for now, tiptap in dashboard takes initialContent, schema changed to fit new json data

--- src/app/actions/bio.ts
+++ src/app/actions/bio.ts
@@ -2,13 +2,13 @@
 
 import { prisma } from '@/lib/prisma';
 import { requireAuth } from '@/lib/session';
-import { RichTextNode } from '@/types/richText';
+import { JSONContent } from '@tiptap/react';
 
-export async function updateBio(paragraphs: RichTextNode[][]) {
+export async function updateBio(tiptapcontent: JSONContent) {
     const { session, error } = await requireAuth()
     if (error) return { error }
 
     await prisma.bio.updateMany({
-        data: { paragraphs },
+        data: { tiptapcontent },
     });
 }
--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,13 +1,21 @@
 import Tiptap from "@/components/editors/Tiptap";
+import { JSONContent } from '@tiptap/react'
+import { prisma } from "@/lib/prisma";
+import { updateBio } from "@/app/actions/bio";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const bio = await prisma.bio.findFirst({
+        select: { tiptapcontent: true },
+    });
+
+    const content: JSONContent = bio?.tiptapcontent as JSONContent || { type: 'doc', content: [] };
 
     return (
         <div style={{ padding: '2rem', textAlign: 'center' }}>
             <h1>Admin Dashboard</h1>
             <p>Welcome back! You’re authenticated ✅</p>
 
-            <Tiptap />
+            <Tiptap initialContent={content} onSave={updateBio}/>
         </div>
     );
 }
