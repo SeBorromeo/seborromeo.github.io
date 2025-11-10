5740ff0fd6c8a00f60a9febf583129eb068018c8
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-11-09 21:46:39 -0500

    removed imports in BioEditorPage for build

--- src/app/admin/dashboard/bioeditor/page.tsx
+++ src/app/admin/dashboard/bioeditor/page.tsx
@@ -1,6 +1,3 @@
-import { prisma } from "@/lib/prisma";
-import { Paragraph } from "@/components/home/Bio/BioParagraph/BioParagraph";
-
 export default async function BioEditorPage() {
 
     return <></>;
