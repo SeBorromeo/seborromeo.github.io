a2fd9a6e0cbd5c958c22464343b323707c6448e9
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-11-12 15:27:50 -0500

    s3 util and added to createProject action

--- src/app/actions/projects.ts
+++ src/app/actions/projects.ts
@@ -2,6 +2,8 @@
 
 import { CreateProjectSchema } from "@/lib/definitions";
 import { prisma } from "@/lib/prisma";
+import { s3 } from "@/lib/s3";
+import { DeleteObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { revalidatePath } from "next/cache";
 import * as z from 'zod'
 
@@ -22,21 +24,51 @@ export async function createProject(formData: FormData) {
 
     const data = validatedFields.data;
 
-    await prisma.projects.create({
-        data: {
-            name: data.name,
-            slug: data.slug,
-            demoUrl: data.demoUrl || null,
-            repoUrl: data.repoUrl,
-            description: data.description,
-            tags: data.tags.split(",").map(tag => tag.trim()),
-            imageUrl: data.imageUrl,
-            order: 0,
-        },
-    });
+    // Convert file to buffer
+    const fileBuffer = Buffer.from(await data.image.arrayBuffer());
+    const fileExt = data.image.name.split(".").pop();
+    const key = `projects/${Date.now()}.${fileExt}`;
 
-    revalidatePath("/");
-    revalidatePath("/projects");
+    try {
+        await s3.send(
+            new PutObjectCommand({
+                Bucket: process.env.AWS_BUCKET_NAME!,
+                Key: key,
+                Body: fileBuffer,
+                ContentType: data.image.type,
+                ACL: "public-read", // optional for public URL
+            })
+        );
 
-    return { success: true };
+        await prisma.projects.create({
+            data: {
+                name: data.name,
+                slug: data.slug,
+                demoUrl: data.demoUrl || null,
+                repoUrl: data.repoUrl,
+                description: data.description,
+                tags: data.tags.split(",").map(tag => tag.trim()),
+                imageUrl: `https://${process.env.AWS_BUCKET_NAME!}.s3.amazonaws.com/${key}`,
+                order: 0,
+            },
+        });
+
+        revalidatePath("/");
+        revalidatePath("/projects");
+
+        return { success: true };
+    } catch (rollbackError) {
+      try {
+        await s3.send(
+            new DeleteObjectCommand({
+                Bucket: process.env.AWS_BUCKET_NAME!,
+                Key: key,
+            })
+        );
+        } catch (rollbackError) {
+            console.error("Rollback failed:", rollbackError);
+        }
+    }
+
+    return { success: false, error: "Upload or database save failed" };
 }
