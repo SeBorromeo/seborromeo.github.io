43ab81a775f1bf94f69ba619e09168419d52a1e4
Sebastian Borromeo <uwg3xs@virginia.edu>
2025-11-14 16:06:17 -0500

    Fixed nested a tag in projects card bug

--- src/components/home/Projects/ProjectsContainer/ProjectCard.tsx
+++ src/components/home/Projects/ProjectsContainer/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import Tags, { TagTheme } from "@/components/ui/Tags/Tags";
 import Image from "next/image";
 import { Project } from "../Projects";
+import { GitHubButton } from "../GitHubButton/GitHubButton";
 
 import styles from '../Projects.module.scss';
 
@@ -10,12 +11,7 @@ export const ProjectCard = ({ project }: { project: Project}) => {
 			<div className={styles.card_content}>
 				<div className={styles.card_image}>
 					{project.imageUrl && <Image src={project.imageUrl} alt={project.name} fill sizes='100%, 100%'/>}
-					{project.demoUrl && project.repoUrl && <a href={project.repoUrl} className={styles.card_github_icon} target="_blank" rel="noopener noreferrer">
-						<svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-							<title>GitHub</title>
-							<path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
-						</svg>
-					</a>}
+					{project.demoUrl && project.repoUrl && <GitHubButton url={project.repoUrl}/> }
 				</div>
 				<div className={styles.card_text_container}>
 					<h2>{project.name}</h2>
